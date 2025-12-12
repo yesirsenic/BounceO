5203b7ec2b275605eb977eb631ae19dc7946f35d
yesirsenic <yesirsenic@naver.com>
2025-12-13 01:09:09 +0900

    touch and rotate commit
    왼쪽/오른쪽 터치 시 방향에 맞게
    원의 곡선따라 바가 움직이는 시스템
    제작

---
+++ BounceO/Assets/Scripts/InputManager.cs
@@ -0,0 +1,25 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public float rotateSpeed = 180f;
+
+
+    [SerializeField]
+    private GameObject Circle;
+
+    private void Update()
+    {
+        if(Mouse.current != null && Mouse.current.leftButton.isPressed)
+        {
+            float touchX = Mouse.current.position.ReadValue().x;
+            float screenCenterX = Screen.width * 0.5f;
+
+            float direction = touchX < screenCenterX ? 1f : -1f;
+
+            Circle.transform.Rotate(0f, 0f, direction * rotateSpeed * Time.deltaTime);
+        }
+    }
+}
